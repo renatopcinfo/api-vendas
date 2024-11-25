9057e362e1ace4f38f2205e8e016e4294f4f922f
Renato Rodrigues <renatopcinfo@gmail.com>
2024-11-25 15:57:26 -0300

    feat: add entity customers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
-// import profileRouter from '@modules/users/routes/profile.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 // import customersRouter from '@modules/customers/routes/customers.routes';
 // import ordersRouter from '@modules/orders/routes/orders.routes';
 
@@ -13,7 +13,7 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
-// routes.use('/profile', profileRouter);
+routes.use('/profile', profileRouter);
 // routes.use('/customers', customersRouter);
 // routes.use('/orders', ordersRouter);
 
